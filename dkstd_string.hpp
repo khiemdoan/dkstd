c627deb96df769402778ae2c1f15c4fffb0c508d
khiemdoancrazy <doankhiem.crazy@gmail.com>
2016-07-06 21:21:12 +0700

    add string, textfile utilities

---
+++ dkstd_string.hpp
@@ -0,0 +1,39 @@
+﻿
+// author: Khiêm Đoàn Hoà
+// created: 2016-03-19
+// modified: 2016-07-06
+
+#ifndef _DKSTD_STRING_
+#define _DKSTD_STRING_
+
+#include "string"
+#include "codecvt"
+
+namespace dkstd {
+
+	// convert std::string to std::wstring
+	std::wstring s2ws(std::string str);
+
+	// convert std::wstring to std::string
+	std::string ws2s(std::wstring wstr);
+}
+
+// convert std::string to std::wstring
+// KhiemDH - 2016-07-06
+inline std::wstring dkstd::s2ws(std::string str)
+{
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+	return converter.from_bytes(str);
+}
+
+// convert std::wstring to std::string
+// KhiemDH - 2016-07-06
+inline std::string dkstd::ws2s(std::wstring wstr)
+{
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+	return converter.to_bytes(wstr);
+}
+
+#endif // !_DK_STRING_H_
---
+++ dkstd_textfile.hpp
@@ -0,0 +1,126 @@
+﻿
+// author: Khiêm Đoàn Hoà
+// created: 2016-07-06
+// modified: 2016-07-06
+
+#ifndef _DKSTD_TEXTFILE_
+#define _DKSTD_TEXTFILE_
+
+#include "fstream"
+#include "vector"
+#include "dkstd_string.hpp"
+
+namespace dkstd {
+
+	// textfile is used to read and write file by utf-8
+	// KhiemDH - 2016-07-06
+	class textfile
+	{
+	public:
+		textfile() {};
+		textfile(std::string sFilePath);
+		textfile(std::wstring sFilePath);
+		~textfile();
+
+		void	open(std::string sFilePath);
+		void	open(std::wstring sFilePath);
+		void	close();
+		
+		void	write_line(std::string sContent);
+		void	write_line(std::wstring sContent);
+
+		std::vector<std::wstring>	get_all_lines();
+
+	private:
+		std::fstream	m_file;
+	};
+}
+
+// constructor
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::textfile(std::string sFilePath)
+{
+	this->open(sFilePath);
+}
+
+// constructor
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::textfile(std::wstring sFilePath)
+{
+	this->open(sFilePath);
+}
+
+// destructor
+// KhiemDH - 2016-07-06
+inline dkstd::textfile::~textfile()
+{
+	this->close();
+}
+
+// open file
+// KhiemDH - 2016-07-06
+inline void dkstd::textfile::open(std::string sFilePath)
+{
+	this->open(dkstd::s2ws(sFilePath));
+}
+
+// open file
+// KhiemDH - 2016-07-06
+inline void dkstd::textfile::open(std::wstring sFilePath)
+{
+	this->close();
+	m_file.open(sFilePath, std::fstream::in | std::fstream::out | std::fstream::app);
+}
+
+// close file
+// KhiemDH - 2016-07-06
+inline void dkstd::textfile::close()
+{
+	if (m_file.is_open() == true) {
+		m_file.close();
+	}
+}
+
+// write a line to end file
+// input: ANSI string
+// KhiemDH - 2016-07-06
+inline void dkstd::textfile::write_line(std::string sContent)
+{
+	if (m_file.good() == true) {
+		m_file.seekp(0, std::fstream::end);
+		if (m_file.tellg() != std::streampos::fpos(0)) {
+			sContent = "\n" + sContent;
+		}
+		m_file << sContent;
+		m_file.clear();		// clear the error flag
+	}
+}
+
+// write a line to end file
+// input: Unicode string
+// KhiemDH - 2016-07-06
+inline void dkstd::textfile::write_line(std::wstring sContent)
+{
+	this->write_line(dkstd::ws2s(sContent));
+}
+
+// read file to std::vector
+// KhiemDH - 2016-07-06
+inline std::vector<std::wstring> dkstd::textfile::get_all_lines()
+{
+	std::string		sLine;
+	std::vector<std::wstring>	vectorContents;
+
+	if (m_file.good() == true) {
+		m_file.seekg(0, std::fstream::beg);
+		while (m_file.eof() == false) {
+			std::getline(m_file, sLine);
+			vectorContents.push_back(dkstd::s2ws(sLine));
+		}
+		m_file.clear();
+	}
+
+	return vectorContents;
+}
+
+#endif // !_DKSTD_TEXTFILE_
