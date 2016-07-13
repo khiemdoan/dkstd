35d2ceddca16ece9581d9974a06e7b6144591197
khiemdoancrazy <doankhiem.crazy@gmail.com>
2016-07-13 11:56:14 +0700

    Add to_lower, to_upper functions for string

--- dkstd_string.hpp
+++ dkstd_string.hpp
@@ -1,21 +1,33 @@
 ﻿
 // author: Khiêm Đoàn Hoà
 // created: 2016-03-19
-// modified: 2016-07-06
+// modified: 2016-07-13
 
 #ifndef _DKSTD_STRING_
 #define _DKSTD_STRING_
 
 #include "string"
 #include "codecvt"
+#include "algorithm"
+#include "memory"
 
 namespace dkstd {
 
-	// convert std::string to std::wstring
 	std::wstring s2ws(std::string str);
-
-	// convert std::wstring to std::string
 	std::string ws2s(std::wstring wstr);
+
+	namespace string {
+
+		template<typename charT>
+		std::basic_string<charT> to_lower(std::basic_string<charT> sInput);
+		template<typename charT>
+		std::basic_string<charT> to_lower(charT* pInput);
+
+		template<typename charT>
+		std::basic_string<charT> to_upper(std::basic_string<charT> sInput);
+		template<typename charT>
+		std::basic_string<charT> to_upper(charT* pInput);
+	}	
 }
 
 // convert std::string to std::wstring
@@ -34,6 +46,52 @@ inline std::string dkstd::ws2s(std::wstring wstr)
 	using convert_type = std::codecvt_utf8<wchar_t>;
 	std::wstring_convert<convert_type, wchar_t> converter;
 	return converter.to_bytes(wstr);
+}
+
+// convert to lower string
+// KhiemDH - 2016-07-13
+template<typename charT>
+inline std::basic_string<charT> dkstd::string::to_lower(std::basic_string<charT> sInput)
+{
+	std::basic_string<charT> sOutput;
+	sOutput.resize(sInput.size());
+	std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::tolower);
+	return sOutput;
+}
+
+// convert to lower string
+// KhiemDH - 2016-07-13
+template<typename charT>
+inline std::basic_string<charT> dkstd::string::to_lower(charT * pInput)
+{
+	std::basic_string<charT> sInput(pInput);
+	std::basic_string<charT> sOutput;
+	sOutput.resize(sInput.size());
+	std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::tolower);
+	return sOutput;
+}
+
+// convert to upper string
+// KhiemDH - 2016-07-13
+template<typename charT>
+inline std::basic_string<charT> dkstd::string::to_upper(std::basic_string<charT> sInput)
+{
+	std::basic_string<charT> sOutput;
+	sOutput.resize(sInput.size());
+	std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::toupper);
+	return sOutput;
+}
+
+// convert to upper string
+// KhiemDH - 2016-07-13
+template<typename charT>
+inline std::basic_string<charT> dkstd::string::to_upper(charT * pInput)
+{
+	std::basic_string<charT> sInput(pInput);
+	std::basic_string<charT> sOutput;
+	sOutput.resize(sInput.size());
+	std::transform(sInput.begin(), sInput.end(), sOutput.begin(), ::toupper);
+	return sOutput;
 }
 
 #endif // !_DK_STRING_H_
