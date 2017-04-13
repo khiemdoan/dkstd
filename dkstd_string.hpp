f494426d7020b521dcd947eb3daa4a3d2070a257
khiemdoancrazy <doankhiem.crazy@gmail.com>
2017-04-13 12:09:57 +0700

    fix s2ws, ws2s function to inline

--- dkstd_string.hpp
+++ dkstd_string.hpp
@@ -52,7 +52,7 @@ namespace dkstd
 
 // convert std::string to std::wstring
 // KhiemDH - 2016-08-14
-std::wstring dkstd::s2ws(std::string str)
+inline std::wstring dkstd::s2ws(std::string str)
 {
     using convert_type = std::codecvt_utf8<wchar_t>;
     std::wstring_convert<convert_type, wchar_t> converter;
@@ -66,7 +66,7 @@ std::wstring dkstd::s2ws(std::string str)
 
 // convert std::wstring to std::string
 // KhiemDH - 2016-08-14
-std::string dkstd::ws2s(std::wstring wstr)
+inline std::string dkstd::ws2s(std::wstring wstr)
 {
     using convert_type = std::codecvt_utf8<wchar_t>;
     std::wstring_convert<convert_type, wchar_t> converter;
